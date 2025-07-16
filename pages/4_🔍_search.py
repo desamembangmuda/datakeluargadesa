1fc9122066dcef00ed457d30d1b421093c744fb0
desamembangmuda <desamembangmuda@gmail.com>
2025-07-16 15:22:34 +0700

    Update 4_🔍_search.py

--- pages/4_🔍_search.py
+++ pages/4_🔍_search.py
@@ -13,6 +13,7 @@ def get_sheet(sheet_name):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     service_account_info = st.secrets["GOOGLE_SERVICE_ACCOUNT"]
     creds = ServiceAccountCredentials.from_json_keyfile_dict(service_account_info, scope)
+    client = gspread.authorize(creds)
     return client.open_by_key("1OjCLeZmypzFvThwmKF2PjheHU2NKedQbw9qzt8joKvs").worksheet(sheet_name)
 
 def ambil_data_anggota():
