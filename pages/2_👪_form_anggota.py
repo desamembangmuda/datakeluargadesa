9d540fdda7dd27c2f3d9f6135aeefd45366d9f87
desamembangmuda <desamembangmuda@gmail.com>
2025-07-16 15:52:18 +0700

    Update 2_👪_form_anggota.py

--- pages/2_👪_form_anggota.py
+++ pages/2_👪_form_anggota.py
@@ -26,8 +26,10 @@ def parse_tanggal_lahir(tgl_str):
             return date.today()
 
 def get_sheet():
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     service_account_info = json.loads(st.secrets["GOOGLE_SERVICE_ACCOUNT"])
     creds = ServiceAccountCredentials.from_json_keyfile_dict(service_account_info, scope)
+    client = gspread.authorize(creds)
     return client.open_by_key("1OjCLeZmypzFvThwmKF2PjheHU2NKedQbw9qzt8joKvs").worksheet("Anggota")
 
 def cek_nik_ganda(nik_baru):
