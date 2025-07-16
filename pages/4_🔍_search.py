367d57fe2a7b4f2471d0e892047af5fdd4b20345
desamembangmuda <desamembangmuda@gmail.com>
2025-07-16 14:31:05 +0700

    Update 4_🔍_search.py

--- pages/4_🔍_search.py
+++ pages/4_🔍_search.py
@@ -10,10 +10,10 @@ from datetime import datetime
 
 # 🔐 Google Sheets Setup
 def get_sheet(sheet_name):
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
-    client = gspread.authorize(creds)
-    return client.open_by_key("1LOv15OJL__vKiok8qmJqPGt4Je4nmxVSV0_a0ed8L5w").worksheet(sheet_name)
+    from oauth2client.service_account import ServiceAccountCredentials
+    service_account_info = json.loads(st.secrets["GOOGLE_SERVICE_ACCOUNT"])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(service_account_info, scope))
+    return client.open_by_key("1OjCLeZmypzFvThwmKF2PjheHU2NKedQbw9qzt8joKvs").worksheet(sheet_name)
 
 def ambil_data_anggota():
     sheet = get_sheet("Anggota")
