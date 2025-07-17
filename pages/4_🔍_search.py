5b8aca6277c8cb0d4c9f81cc3fb2a2585001fa30
desamembangmuda <desamembangmuda@gmail.com>
2025-07-17 09:31:30 +0700

    Update 4_🔍_search.py

--- pages/4_🔍_search.py
+++ pages/4_🔍_search.py
@@ -125,7 +125,9 @@ if no_kk_input:
                     if k.startswith("edit_"):
                         del st.session_state[k]
                 st.switch_page("pages/2_👪_form_anggota.py")
-
+    try:
+        sheet = get_sheet("Anggota")
+        st.success("✅ Berhasil ambil sheet Anggota")
     except Exception as e:
         st.error(f"❌ Gagal memuat data: {e}")
 
