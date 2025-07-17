81999bb2f1736663e1884d057733f85b7593a4e9
desamembangmuda <desamembangmuda@gmail.com>
2025-07-17 07:57:42 +0700

    Update 4_🔍_search.py

--- pages/4_🔍_search.py
+++ pages/4_🔍_search.py
@@ -11,7 +11,7 @@ if "login" not in st.session_state or not st.session_state["login"]:
 # 🔐 Google Sheets Setup
 def get_sheet(sheet_name):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    service_account_info = st.secrets["google_service_account"]
+    service_account_info = st.secrets["GOOGLE_SERVICE_ACCOUNT"]
     creds = ServiceAccountCredentials.from_json_keyfile_dict(service_account_info, scope)
     client = gspread.authorize(creds)
     return client.open_by_key("1OjCLeZmypzFvThwmKF2PjheHU2NKedQbw9qzt8joKvs").worksheet(sheet_name)
